b415a504c323b3a14715d194d4191f08d9c9b247
amarkatnsemwal <er.amar429@gmail.com>
2023-11-01 07:41:19 +0530

    'changes'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AddUser from './components/UserModule/AddUser';
 function App() {
   return (
     <div className="App">
-      hi development
+      <AddUser></AddUser>
     </div>
   );
 }
---
+++ src/LayOut/AdminLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminLayout(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AdminLayout;
